502bb2d456b1a1d48cc09cb053397919da28e701
Jeff Simonitto <jsimonitto@gmail.com>
2019-08-03 15:31:53 -0400

    PATCH /books/:bookId

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -43,8 +43,34 @@ function routes(Book) {
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
       return res.json(book);
+    })
+    .patch((req, res) => {
+      const { book } = req;
+
+      // eslint-disable-next-line no-underscore-dangle
+      if (req.body._id) {
+        // eslint-disable-next-line no-underscore-dangle
+        delete req.body._id;
+      }
+
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
     });
 
   return bookRouter;
