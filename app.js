aa5f07acbc42b1c0311295b22e1f0fd77e8ca579
Jeff Simonitto <jsimonitto@gmail.com>
2019-08-03 12:39:12 -0400

    GET /api/books

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+mongoose.connect('mongodb://localhost/test');
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    const response = { hello: 'This is my API!' };
-
-    res.json(response);
+    Book.find((err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
   });
 
 app.use('/api', bookRouter);
