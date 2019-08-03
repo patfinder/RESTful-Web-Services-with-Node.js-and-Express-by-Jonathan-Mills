1584f486adf407da77dcf324a999b59bc06393ce
Jeff Simonitto <jsimonitto@gmail.com>
2019-08-03 13:38:56 -0400

    refactor routes into separate file

--- app.js
+++ app.js
@@ -1,43 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 const app = express();
-const bookRouter = express.Router();
+
 const port = process.env.PORT || 3000;
 mongoose.connect('mongodb://localhost/test');
-const Book = require('./models/bookModel');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
 
 app.use('/api', bookRouter);
 
